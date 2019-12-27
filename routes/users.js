3f2181d5f7623efc0bc09037cdce3deda48aa414
Anojan <anojan010@gmail.com>
2019-12-27 10:31:33 +0530

    First commit.

---
+++ routes/users.js
@@ -0,0 +1,69 @@
+const auth = require("../middleware/auth");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const { User, validate } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password"); // user will be assigne by middleware(auth) function to the request.
+  res.send(user);
+});
+
+router.get("/", async (req, res) => {
+  const users = await User.find().sort("name");
+  res.send(users);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  user = await user.save();
+
+  const token = user.generateAuthToken();
+
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
+      isGold: req.body.isGold,
+      name: req.body.name,
+      phone: req.body.phone
+    },
+    { new: true }
+  );
+
+  if (!customer)
+    return res
+      .status(404)
+      .send("The customer with the given ID was not found.");
+
+  res.send(customer);
+});
+
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+
+  if (!customer)
+    return res.status(404).send("The customer with the given ID was not found");
+
+  res.send(customer);
+});
+
+module.exports = router;
